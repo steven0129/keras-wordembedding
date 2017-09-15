7423b3f388d82976fbecd05ae0b728dbddbf8492
steven0129 <h3584935@gmail.com>
2017-09-15 20:04:12 +0800

    fix bug

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, KFold
 import lib.word as word
 import lib.ml as ml
 import numpy as np
@@ -13,20 +13,22 @@ indices = np.arange(data.shape[0])
 np.random.shuffle(indices)
 X = data[indices]
 Y = labels[indices]
-kfold = StratifiedKFold(n_splits=10, shuffle=True)
+kf = KFold(n_splits=10, shuffle=True)
 
-for train, test in kfold.split(X, Y):
+for trainIndex, testIndex in kf.split(X):
     # build model
     myModel = ml.dlModel(inputDim=data.shape[1], activation='softmax',
                          loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # train model
-    myModel.fit(X[train], Y[train], epochs=400,
-                validation_split=0.2, verbose=0)
+    myModel.fit(X[trainIndex], Y[trainIndex], epochs=400,
+                validation_split=0.2, verbose=1)
 
     # evaluate model
-    scores = myModel.evaluate(X[test], Y[test], verbose=0)
+    scores = myModel.evaluate(X[testIndex], Y[testIndex], verbose=0)
     cvscores.append(scores[1] * 100)
+
+    # print accuracy
     print("%s: %.f%%" % (myModel.metrics_names[1], scores[1] * 100))
 
 print("%.2f (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
